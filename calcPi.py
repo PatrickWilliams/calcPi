4cdd87b1975bf2779c0643c50e7cf332dcf17101
Patrick Williams <pat.williams101@gmail.com>
2017-04-02 17:58:40 -0400

    added calculation functionality

--- calcPi.py
+++ calcPi.py
@@ -1,1 +1,30 @@
-hello pls ignore this
+#!python
+import math
+
+
+N = input("Enter the number of digits of pi you would like to compute: ")
+
+runs = math.ceil(N/math.log(2))
+
+a_0 = 1.0
+b_0 = 1.0/(2.0**(1.0/2.0))
+t_0 = 1.0/4.0
+p_0 = 1.0
+
+for x in xrange(1,N):
+
+	a = (a_0 + b_0)/2
+	b = (a_0*b_0)**(1.0/2.0)
+	t = t_0 - p_0*(a_0 - a)**2
+	p = 2*p_0
+
+	pi = ((a_0 + b_0)**2)/(4*t)
+	print pi
+	
+	a_0 = a
+	b_0 = b
+	t_0 = t
+	p_0 = p
+
+print 'Pi to %d number of digits is: ' % (N)
+print "%0" N "d\n" % pi	 
